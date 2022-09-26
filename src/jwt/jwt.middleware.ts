74c0518c5e2074f31963e8fb862ba2e97a43102a
god0331 <god010331@naver.com>
2022-09-26 22:30:56 +0900

    user를 헤더에서 리퀘스트로 보냄

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,9 +1,25 @@
-import { NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction } from 'express';
+import { UsersService } from '../users/users.service';
+import { JwtService } from './jwt.service';
 
+@Injectable()
 export class JwtMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log(req.headers);
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    if ('x-jwt' in req.headers) {
+      const token = req.headers['x-jwt']; //headers에 있는 token을 저장한다. 이후 해독decrypt
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === 'object' && decoded.hasOwnProperty('id')) {
+        try {
+          const user = await this.userService.findById(decoded['id']);
+          req['user'] = user;
+        } catch (e) {}
+      }
+    }
     next();
   }
 }
